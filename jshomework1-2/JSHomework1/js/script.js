97672d7fe42bf513cd91720cecc6c1ae152caa27
Malytin <mmalytin@mail.ru>
2016-03-21 00:35:54 +0200

    jshomework1-2

---
+++ jshomework1-2/JSHomework1/js/script.js
@@ -0,0 +1,12 @@
+﻿function pow(x,n) {
+    var result = 1;
+    var x = prompt ( 'Введите число','' );
+    var n = prompt ( 'Введите степень', '' );
+    for (var i = 0; i<n; i++) {
+        result *= x;
+    }
+    console.log('result = ', result);
+    return result;
+}
+
+pow();
