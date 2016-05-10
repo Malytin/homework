9071221a6cea4bdf563661197bceaa72095a9254
Malytin <mmalytin@mail.ru>
2016-05-11 02:02:26 +0300

    jshomework7-8

---
+++ jshomework7-8/Part1/js/script.js
@@ -0,0 +1,76 @@
+﻿$(function (){
+    var $menu = $('.menu');
+    var $left = $('.menu li:nth-child(1)');
+    var $center = $('.menu li:nth-child(2)');
+    var $right = $('.menu li:nth-child(3)');
+    var $border = $('.submenu li,.submenu_center li,.submenu_right li');
+    var $nonShadow = $('.submenu p,.submenu_center p,.submenu_right p');
+    $nonShadow.on('click', function () {
+        var $item = $('.submenu li,.submenu_center li,.submenu_right li');
+        $item
+            .css({
+                border: 'none',
+                height: '28px'
+            })
+    });
+    $border.on('click', function () {
+        $(this)
+            .css({
+                height: '26px',
+                border: '1px solid #4eabda'
+            })
+    });
+
+    $left.on('click',function () {
+        var $fuck = $('.submenu');
+        $fuck
+            .css({
+                display: 'block',
+                zindex: '2'
+            });
+        var $fuckCenter = $('.submenu_center');
+        $fuckCenter
+            .css({
+                display: 'none'
+            });
+        var $fuckRight = $('.submenu_right');
+        $fuckRight
+            .css({
+                display: 'none'
+            });
+    });
+    $center.on('click',function () {
+        var $fuckCenter = $('.submenu_center');
+        $fuckCenter
+            .css({
+                display: 'block'
+            });
+        var $fuck = $('.submenu');
+        $fuck
+            .css({
+                display:'none'
+            });
+        var $fuckRight = $('.submenu_right');
+        $fuckRight
+            .css({
+                display: 'none'
+            });
+    });
+    $right.on('click',function () {
+        var $fuckRight = $('.submenu_right');
+        $fuckRight
+            .css({
+                display: 'block'
+            });
+        var $fuck = $('.submenu');
+        $fuck
+            .css({
+                display:'none'
+            });
+        var $fuckCenter = $('.submenu_center');
+        $fuckCenter
+            .css({
+                display: 'none'
+            });
+    });
+});
---
+++ jshomework7-8/Part2/js/script.js
@@ -0,0 +1,107 @@
+﻿$(function () {
+    var $body = $('div');
+    var $main = $('form');
+    var $first = $('#firstname');
+    var $last = $('#lastname');
+    var $address =$('#address');
+    var $submit =$('input[type="submit"]');
+    $first.hover(function () {
+        var $textFirst = $('.first');
+        $textFirst
+            .css({
+                display: 'block'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+        var $textLast = $('.second');
+        $textLast
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+        var $textAddress = $('.third');
+        $textAddress
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+    });
+    $last.hover(function () {
+        var $textLast = $('.second');
+        $textLast
+            .css({
+                display: 'block'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+        var $textFirst = $('.first');
+        $textFirst
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+        var $textAddress = $('.third');
+        $textAddress
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+    });
+    $address.hover(function () {
+        var $textAddress = $('.third');
+        $textAddress
+            .css({
+                display: 'block'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+        var $textFirst = $('.first');
+        $textFirst
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+        var $textLast = $('.second');
+        $textLast
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+    });
+    $submit.on('click',function (e) {
+        var $showAll = $('.second,.first,.third');
+        e.preventDefault();
+        $showAll
+            .css({
+                display: 'block'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+    });
+    $body.hover(function () {
+        var $noneAll = $('.second,.first,.third');
+        $noneAll
+            .css({
+                display: 'none'
+            })
+            .animate({
+                opacity:"1"
+            },2000);
+    })
+});
