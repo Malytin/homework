5658eaa6c43ad6728a849256ffdc39f3bb2bd142
Malytin <mmalytin@mail.ru>
2016-05-23 23:32:42 +0300

    jshomework13-14

---
+++ jshomework13-14/js/QBase.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+ var QBase = {
+    question:  [
+        {
+            q:'Что такое JavaScript',
+                answer: [
+                    {text:'Приложение'},
+                    {text:'Название языка'},
+                    {text:'Язык програмирования'}
+                        ]
+        },
+        {
+            q:'Какое значение по-умолчанию для стиля position',
+                answer: [
+                    {text:'Inline'},
+                    {text:'Static'},
+                    {text:'Absolute'}
+                ]
+        }
+    ]
+};
+
+localStorage.setItem('QBase',JSON.stringify(QBase));
+
+var QBase = localStorage.getItem('QBase');
+QBase = JSON.parse(QBase);
+
---
+++ jshomework13-14/js/script.js
@@ -0,0 +1,65 @@
+﻿'use strict';
+$(function () {
+    var $html = $('html');
+    var $body = $('body');
+    var $answerLabel = $('.answerLabel');
+    var html = $('#test').html();
+    var tmpl =  $('#test').html();
+    tmpl = _.template(tmpl);
+
+    var content = tmpl(html,{
+        QBase: QBase
+    });
+
+
+    $('body').append(content);
+
+    for(var i=0;i<QBase.question.length; i++){
+        $('.answer_checkbox'+i).addClass('disabled'+i);
+        $('.disabled0').on('change',function(){
+            if(this.checked){
+                $(this).removeClass('disabled0');
+                $('.disabled0').attr('disabled','disabled');
+            } else {
+                $(this).addClass('disabled0');
+                $('.disabled0').removeAttr('disabled');
+            }
+        });
+        $('.disabled1').on('change',function(){
+            if(this.checked){
+                $(this).removeClass('disabled1');
+                $('.disabled1').attr('disabled','disabled');
+            } else {
+                $(this).addClass('disabled1');
+                $('.disabled1').removeAttr('disabled');
+            }
+        });
+    }
+
+    var check1;
+    var check2;
+    var check3;
+    $('#chBox_0_2').on('click', function () {
+        check1 = true
+    });
+    $('#chBox_1_1').on('click', function () {
+        check2 = true
+    });
+
+
+
+
+    $('.answerButton').on('click', function (e) {
+
+        e.preventDefault();
+        if (check1 && check2 == true) {
+            alert('И это правильный ответ');
+            location.reload();
+        }   else {
+            alert('Это неправильный ответ');
+            location.reload();
+        }
+
+
+    });
+});
